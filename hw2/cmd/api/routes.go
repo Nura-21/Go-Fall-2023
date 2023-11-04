e550a14c94af11913833cc7aa58e43b3a2eec201
Nura-21 <n_turdalin@kbtu.kz>
2023-11-05 00:15:28 +0600

    Chapter 11

--- hw2/cmd/api/routes.go
+++ hw2/cmd/api/routes.go
@@ -17,5 +17,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/sgcameras/:id", app.showSGCameraHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/sgcameras/:id", app.updateSGCameraHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/sgcameras/:id", app.deleteSGCameraHandler)
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
